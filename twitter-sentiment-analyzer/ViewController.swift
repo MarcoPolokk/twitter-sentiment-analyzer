5a17e68dda287a52743cd871cd07763db58dbd90
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-03-01 22:15:41 +0100

    Made a fetch request for Twitter tweets data

--- twitter-sentiment-analyzer/ViewController.swift
+++ twitter-sentiment-analyzer/ViewController.swift
@@ -13,9 +13,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var sentimentLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let swifter = Swifter(consumerKey: "ldYK2mF8vYt4tpTOHLhEBxMWe", consumerSecret: "1FORawqFRG3K6tqqFgcEaOQ6cfkR3ruF2oSfNxT9upxdkUaOir")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            print(results)
+        } failure: { (error) in
+            print("There was an error with Twitter API Request, \(error)")
+        }
+
     }
 
     @IBAction func predictPressed(_ sender: UIButton) {
