a009ee91c5ec4faa9f6a692114459393c07d689b
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-03-02 18:06:07 +0100

    Made a sentiment counter for batch predictions

--- twitter-sentiment-analyzer/ViewController.swift
+++ twitter-sentiment-analyzer/ViewController.swift
@@ -22,20 +22,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let prediction = try! sentimentClassifier.prediction(text: "")
-        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
             
-            var tweets = [String]()
+            var tweets = [TweetSentimentClassifierInput]()
             
             for i in 0..<100 {
                 if let tweet = results[i]["full_text"].string {
-                    tweets.append(tweet)
+                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                    tweets.append(tweetForClassification)
                 }
             }
             
+            do {
+                let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                var sentimentScore = 0
+                
+                for pred in predictions {
+                    let sentiment = pred.label
+                    
+                    if sentiment == "Pos" {
+                        sentimentScore += 1
+                    } else if sentiment == "Neg" {
+                        sentimentScore -= 1
+                    }
+                }
+                print(sentimentScore)
+                
+            } catch {
+                print(error)
+            }
             
-            print(results)
         } failure: { (error) in
             print("There was an error with Twitter API Request, \(error)")
         }
