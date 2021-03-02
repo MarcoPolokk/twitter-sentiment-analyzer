cc0cc8d5b0c9345288aa12df57966a45424aa159
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-03-02 16:05:06 +0100

    Parsing tweet data into JSON by SwiftyJSON

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'twitter-sentiment-analyzer' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for twitter-sentiment-analyzer
+  pod 'SwiftyJSON'
+
+end
--- twitter-sentiment-analyzer/ViewController.swift
+++ twitter-sentiment-analyzer/ViewController.swift
@@ -7,25 +7,41 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var sentimentLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let sentimentClassifier = TweetSentimentClassifier()
+    
     let swifter = Swifter(consumerKey: "ldYK2mF8vYt4tpTOHLhEBxMWe", consumerSecret: "1FORawqFRG3K6tqqFgcEaOQ6cfkR3ruF2oSfNxT9upxdkUaOir")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let prediction = try! sentimentClassifier.prediction(text: "")
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results, metadata) in
+            
+            var tweets = [String]()
+            
+            for i in 0..<100 {
+                if let tweet = results[i]["full_text"].string {
+                    tweets.append(tweet)
+                }
+            }
+            
+            
             print(results)
         } failure: { (error) in
             print("There was an error with Twitter API Request, \(error)")
         }
-
+        
     }
-
+    
     @IBAction func predictPressed(_ sender: UIButton) {
     }
     
